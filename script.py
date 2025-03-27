1b6ca06d1f6e4fd1eee312350eaf4cc463fb8748
danilaltd <44562824+danilaltd@users.noreply.github.com>
2025-03-27 22:12:08 +0300

    Update script.py

--- script.py
+++ script.py
@@ -11,13 +11,16 @@ combinations = filter(lambda x: x[0] != x[1] and x[1] != x[2], combinations)
 
 for combination in combinations:
     a = ''.join(combination)
-    if (a[0] in "абвгдеё"):
+    if (a[0] in "абвгдеёжз"):
         continue
     url = f"https://pass.rw.by/ru/ajax/autocomplete/search/?term={a}"
     response = requests.get(url)
     if response.status_code == 200:
-        data = response.json()
-        print(a, len(data))
+        try:
+            data = response.json()
+            print(a, len(data))
+        except requests.exceptions.JSONDecodeError as e:
+            print(f"Ошибка декодирования JSON: {e}")
 
     else:
         print(f"Ошибка при выполнении запроса: {response.status_code}")
