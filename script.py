d52b246d05b9a3fb96c474dd4a4335725c21b2b3
danilaltd <44562824+danilaltd@users.noreply.github.com>
2025-03-28 13:53:08 +0300

    начиная с л

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ combinations = filter(lambda x: x[0] != x[1] and x[1] != x[2], combinations)
 
 for combination in combinations:
     a = ''.join(combination)
-    if (a[0] in "абвгдеёжз"):
+    if (a[0] in "абвгдеёжзийк"):
         continue
     url = f"https://pass.rw.by/ru/ajax/autocomplete/search/?term={a}"
     response = requests.get(url)
